1f7320fb9f362599ad2ba448338786e7e4e56269
martin <martin.aliaga1@mail.udp.cl>
2023-07-20 20:01:48 -0400

    added vite build files

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 
 const demoUtils = require('./utils/demoUtils');
 dotenv.config();
@@ -9,12 +11,16 @@ dotenv.config();
 const app = express();
 
 app.use(cors({ origin: "*"}));
-
+app.use(express.static(path.join(__dirname, 'build')));
 
 mongoose.connect(process.env.MONGO_CONNECT_URL);
 
 app.use("/api/teams", require("./routes/teams"));
 app.use("/api/players", require("./routes/players"));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 demoUtils.checkAllHubs();
 setInterval(demoUtils.checkAllHubs, 1000 * 60 * 5);
